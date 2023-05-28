920602feae91c3b9cfe211760b24a6b470e4f3a9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-28 11:42:29 +0530

    recursion

---
+++ src/recursion/PowerOfX.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class PowerOfX {
+
+    public static int powerOfX(int x, int n){
+        if (n == 0) {
+            return 1;
+        }
+        int power = x * powerOfX(x, n-1);
+        return power;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(powerOfX(2, 20));
+    }
+}
