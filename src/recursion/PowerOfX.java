3ee94e4ad95fb9c195ae8e16bb74cd99c4965bf9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-28 11:52:17 +0530

    recursion

--- src/recursion/PowerOfX.java
+++ src/recursion/PowerOfX.java
@@ -10,6 +10,15 @@ public class PowerOfX {
         return power;
     }
 
+    // OPTIMIZED Time Complexity O(logN)
+
+    public static int powerOfXOptimized(int x, int n){
+        if (n == 0) {
+            return 1;
+        }
+        int power = x * powerOfX(x, n-1);
+        return power;
+    }
 
     public static void main(String[] args) {
         System.out.println(powerOfX(2, 20));
