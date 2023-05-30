b4fdf6bcdf40a034a00e9b15acab46944a70da24
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-30 12:18:23 +0530

    recursion

--- src/recursion/BinaryString.java
+++ src/recursion/BinaryString.java
@@ -4,20 +4,35 @@ public class BinaryString {
 
     public static void printBinaryString(int n, String str, int lastPlace){
 
-        if (n == 0) {
+// Ques: String Does not contain consecutive 1s
+
+//        if (n == 0) {
+//            System.out.println(str);
+//            return;
+//        }
+//
+//        if (lastPlace == 1){
+//            printBinaryString(n-1, str+"0", 0);
+//        } else {
+//            printBinaryString(n-1, str+"0", 0);
+//            printBinaryString(n-1, str+"1", 1);
+//        }
+
+
+// Ques: String Does not contain consecutive 0s
+        if(n == 0){
             System.out.println(str);
             return;
         }
-
-        if (lastPlace == 1){
+        if(lastPlace == 1) {
+            printBinaryString(n-1, str+"1", 1);
             printBinaryString(n-1, str+"0", 0);
         } else {
-            printBinaryString(n-1, str+"0", 0);
             printBinaryString(n-1, str+"1", 1);
         }
     }
 
     public static void main(String[] args) {
-        printBinaryString(3, "", 0);
+        printBinaryString(3, "", 1);
     }
 }
