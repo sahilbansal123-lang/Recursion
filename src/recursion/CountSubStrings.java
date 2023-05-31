289cbd3e2534137bf87d01d32ee18c455d1f1658
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-31 09:38:35 +0530

    recursion

--- src/recursion/CountSubStrings.java
+++ src/recursion/CountSubStrings.java
@@ -1,4 +1,25 @@
 package recursion;
 
 public class CountSubStrings {
+
+    public static int countSubstrs(String str, int i, int j, int n){
+
+        if (n == 1){
+            return 1;
+        }
+        if (n <= 0) {
+            return 0;
+        }
+        int res = countSubstrs(str,i+1,j,n-1)
+                 +countSubstrs(str,i,j-1,n-1)
+                 -countSubstrs(str,i+1,j-1,n-2);
+
+        if (str.charAt(i) == str.charAt(j)){
+            res++;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSubstrs("abcab", 0, 4, 5));
+    }
 }
